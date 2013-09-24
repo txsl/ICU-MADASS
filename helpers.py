afb854257a3c978b2cdbd02c34a95b61d4b022b6
Thomas Lim <txl11@ic.ac.uk>
2013-09-24 01:18:00 +0100

    Moved a large chunk of code around, but we are now able to match for all Depts! Also fixed the similarity scorer so scores are normalised (stops a runaway group expanding).

--- helpers.py
+++ helpers.py
@@ -1,5 +1,84 @@
+from __future__ import division
 import numpy
 import copy
+from db import db
+from collections import OrderedDict
+import math
+import pickle
+import csv
+
+def makeBabies(DeptId,  DeptName):
+
+	work = stuff(db)
+
+	work.BuildInterests(DeptId)
+
+	unallocatedFreshers = work.returnFreshers(DeptId)
+	fams, familiesWithSpace = work.StartFamilies(DeptId)
+
+	print fams
+	print len(unallocatedFreshers), len(fams), len(unallocatedFreshers) / (len(fams))
+	maxFamSize = math.ceil(len(unallocatedFreshers) / (len(fams)))
+
+	print 'Max Family Size is', maxFamSize
+
+	while unallocatedFreshers:
+		scores = {}
+
+		for fam in familiesWithSpace:
+			for fresh in unallocatedFreshers:
+				score = work.CalcSimilarity(fam, fams[fam], fresh)
+				scores[(fam, fresh)] = score
+
+		scores = OrderedDict(sorted(scores.items(), key=lambda t: t[1])) # Let's order them, so we can match the best score
+
+		allocated = scores.popitem()
+
+		famId = allocated[0][0]
+		fresherId = allocated[0][1]
+
+		print allocated
+
+		fams[famId].append(fresherId)
+
+		unallocatedFreshers.remove(fresherId)
+
+		print len(fams[famId]), maxFamSize
+		if len(fams[famId]) == maxFamSize:
+			familiesWithSpace.remove(famId)
+			print 'removing family'
+
+
+		# order = []
+		# for key, var in scores:
+		# 	order.append(key)
+
+	# print scores
+	print fams
+
+	# convert fams to a list form for csv printing
+
+	toPrint = []
+
+	for rents, children in fams.iteritems():
+		toPrint.append(list(rents) + children)
+
+	print toPrint
+
+	header = ['parent 1', 'parent 2']
+	for i in range(1, int(maxFamSize) + 1):
+		header.append('child ' + str(i))
+
+	fname = ''
+	for l in DeptName:
+		fname = fname + l
+	fname = fname + '.csv'
+
+	with open(fname, 'wb+') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(header)
+		for line in toPrint:
+			writer.writerow(line)
 
 def AreTheyThere(haystack, needle):
 	for item in haystack:
@@ -31,7 +110,7 @@ class stuff:
 		return res
 
 	def BuildInterests(self, DeptId): # Builds interests up for an entire Department in to a dict, but tupled to split between parents and children (makes it easier later)
-		Pinterests, Cinterests = {}, {}
+		Pinterests, Cinterests = {}, {} # Like this as a hangover, in case we want to return these rather than just keep them
 		# For the parents
 		people = self.db.ParentPeople.filter(self.db.ParentPeople.DepartmentId==DeptId)
  
@@ -47,9 +126,6 @@ class stuff:
 		self.Interests = copy.deepcopy(Pinterests)
 
 		self.Interests.update(Cinterests)
-
-
-		return Pinterests, Cinterests
 
 	def StartFamilies(self, DeptId): #This function works on the assumtion that there are no 'dud' parents. Data cleaned by the wonderful @lsproc
 		parents = self.db.ParentPeople.filter(self.db.ParentPeople.DepartmentId==DeptId)
@@ -78,6 +154,13 @@ class stuff:
 		combined = numpy.add(self.Interests[parents[0]], self.Interests[parents[1]])
 		for child in children:
 			combined = numpy.add(combined, self.Interests[child])
-		# combined = numpy.divide(combined, 2 + len(children))
+		combined = numpy.divide(combined, 2 + len(children))
 		return numpy.dot(combined, self.Interests[fresher])
+
+	def ReturnDepts(self):
+		depts = self.db.Departments.all()
+		r = []
+		for d in depts:
+			r.append((d.DepartmentId, d.DepartmentNameTypeName))
+		return r
 		
--- process.py
+++ process.py
@@ -1,83 +1,12 @@
-from __future__ import division
 from db import db
 from helpers import *
-from collections import OrderedDict
-import math
-import pickle
-import csv
 
-work = stuff(db)
+helper = stuff(db)
 
+depts = helper.ReturnDepts()
 
-
-work.BuildInterests(4)
-# print len(p), len(c)
-
-# work.CalcFamInterests(fams[0], [])
-
-# work.CalcSimilarity(fams[0],129093)
-
-unallocatedFreshers = work.returnFreshers(4)
-fams, familiesWithSpace = work.StartFamilies(4)
-
-print fams
-print len(unallocatedFreshers), len(fams), len(unallocatedFreshers) / (len(fams))
-maxFamSize = math.ceil(len(unallocatedFreshers) / (len(fams)))
-
-print 'Max Family Size is', maxFamSize
-
-while unallocatedFreshers:
-	scores = {}
-
-	for fam in familiesWithSpace:
-		for fresh in unallocatedFreshers:
-			score = work.CalcSimilarity(fam, fams[fam], fresh)
-			scores[(fam, fresh)] = score
-
-	scores = OrderedDict(sorted(scores.items(), key=lambda t: t[1])) # Let's order them, so we can match the best score
-
-	allocated = scores.popitem()
-
-	famId = allocated[0][0]
-	fresherId = allocated[0][1]
-
-	print allocated
-
-	fams[famId].append(fresherId)
-
-	unallocatedFreshers.remove(fresherId)
-
-	print len(fams[famId]), maxFamSize
-	if len(fams[famId]) == maxFamSize:
-		familiesWithSpace.remove(famId)
-		print 'removing family'
-
-
-	# order = []
-	# for key, var in scores:
-	# 	order.append(key)
-
-# print scores
-print fams
-
-# convert fams to a list form for csv printing
-
-toPrint = []
-
-for rents, children in fams.iteritems():
-	toPrint.append(list(rents) + children)
-
-print toPrint
-
-header = ['parent 1', 'parent 2']
-for i in range(1, int(maxFamSize) + 1):
-	header.append('child ' + str(i))
-
-with open('output.csv', 'wb+') as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerow(header)
-	for line in toPrint:
-		writer.writerow(line)
-
+for d in depts:
+	print 'Making babies in', d[1]
+	makeBabies(d[0], d[1])
 
 exit('All done!')

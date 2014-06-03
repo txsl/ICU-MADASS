8402f8baccf629237063b259921ca09f25ca4cb3
Thomas Lim <txl11@ic.ac.uk>
2014-06-03 23:40:52 +0100

    Now we can make babies for any selection of Departments, or all

--- process.py
+++ process.py
@@ -1,14 +1,27 @@
 from db import db
 from helpers import *
+import argparse
+
+parser = argparse.ArgumentParser(description='ICU MADASS Matching System')
+
+parser.add_argument('-d', '--depts', type=long, nargs='*', help='Optional for only processing certain departments')
+
+args = vars(parser.parse_args())['depts']
 
 helper = stuff(db)
+depts = dict(helper.ReturnDepts())
 
-depts = helper.ReturnDepts()
 
-printParents()
+if args:
+    print "Making babies for specified Departments..."
+    for selectd in args:
+        print 'Making babies in', depts[selectd]
+        makeBabies(selectd, depts[selectd])
+else:
+    print "Making babies for the entire University. Hold on tight: it's gona get warm..."
+    for ids, dnames in depts.iteritems():
+        print 'Making babies in', dnames
+        makeBabies(ids, dnames)
 
-for d in depts:
-	print 'Making babies in', d[1]
-	makeBabies(d[0], d[1])
 
 exit('All done!')

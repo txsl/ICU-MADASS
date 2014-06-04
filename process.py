553763a9614fb37fcc1847c8948633e0de65f42f
Thomas Lim <txl11@ic.ac.uk>
2014-06-04 20:31:20 +0100

    Can list departments now

--- process.py
+++ process.py
@@ -1,17 +1,31 @@
+import argparse
+from prettytable import PrettyTable
+
 from db import db
 from helpers import *
-import argparse
 
 parser = argparse.ArgumentParser(description='ICU MADASS Matching System')
 
 parser.add_argument('-d', '--depts', type=long, nargs='*', help='Optional for only processing certain departments')
 parser.add_argument('-a', '--anal', action='store_true', help='Raise this flag to run some analysis on the computed data')
 parser.add_argument('--dry', action='store_true', help='Raise this flag to use data calculated and stored from before')
+parser.add_argument('-l', '--list', action='store_true', help='List all Departments')
 
 args = vars(parser.parse_args())
 
 helper = stuff(db)
 depts = dict(helper.ReturnDepts())
+
+if args['list']:
+    print 'Departments:'
+    
+    x = PrettyTable(["Department", "ID"])
+    for ids, dnames in depts.iteritems():
+        x.add_row([dnames, ids])
+    print x
+    
+    exit()
+
 
 
 if args['depts']:
